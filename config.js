1a9b8082d762f10b3f9ba3372b601db59edb99d9
not a cat <32406395+the-non-feline@users.noreply.github.com>
2020-05-01 17:03:53 -0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,18 @@
+var config = {
+    "host": "https://lcusd-net.tk/",
+    "post-report": "api/report",
+    "post-feedback": "api/feedback",
+    "post-bug": "api/bug", 
+    "get-blacklist": "api/get_blacklist", 
+    "info-page": "info",
+    "chrome-webstore-link": "https://http.cat/404",
+    "top-domains": [
+        "google.com",
+        "youtube.com",
+        "wikipedia.org",
+        "facebook.com",
+        "amazon.com"
+    ],
+    "openpagerank-api-key":"c0000oo0kcsso8gog8skcsssskwokw808sg4ccoc"
+    //Derek (derek.l.jiang@gmail.com)'s API key
+} 
